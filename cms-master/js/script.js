f31c4e9f39e9562b03a9be8f9d8f5228e5843988
MatiPL86 <31951249+MatiPL86@users.noreply.github.com>
2017-12-01 16:02:37 +0100

    tab-scroll

--- cms-master/js/script.js
+++ cms-master/js/script.js
@@ -1,65 +1,69 @@
 $(function() {
-    
-        var Page = (function() {
 
-            var $nav = $( '#nav-dots > span' ),
-                slitslider = $( '#slider' ).slitslider( {
-                    onBeforeChange : function( slide, pos ) {
+  $("#gallery").unitegallery({
+    tiles_type: "nested"
+  });
 
-                        $nav.removeClass( 'nav-dot-current' );
-                        $nav.eq( pos ).addClass( 'nav-dot-current' );
+  var Page = (function() {
 
-                    }
-                } ),
+    var $nav = $( '#nav-dots > span' ),
+      slitslider = $( '#slider' ).slitslider( {
+        onBeforeChange : function( slide, pos ) {
 
-                init = function() {
+          $nav.removeClass( 'nav-dot-current' );
+          $nav.eq( pos ).addClass( 'nav-dot-current' );
 
-                    initEvents();
-                    
-                },
-                initEvents = function() {
+        }
+      } ),
 
-                    $nav.each( function( i ) {
-                    
-                        $( this ).on( 'click', function( event ) {
-                            
-                            var $dot = $( this );
-                            
-                            if( !slitslider.isActive() ) {
+      init = function() {
 
-                                $nav.removeClass( 'nav-dot-current' );
-                                $dot.addClass( 'nav-dot-current' );
-                            
-                            }
-                            
-                            slitslider.jump( i + 1 );
-                            return false;
-                        
-                        } );
-                        
-                    } );
+        initEvents();
 
-                };
+      },
+      initEvents = function() {
 
-                return { init : init };
+        $nav.each( function( i ) {
 
-        })();
+          $( this ).on( 'click', function( event ) {
 
-        Page.init();
+            var $dot = $( this );
 
-        /**
-         * Notes: 
-         * 
-         * example how to add items:
-         */
+            if( !slitslider.isActive() ) {
 
-        /*
-        
-        var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
-        
-        // call the plugin's add method
-        ss.add($items);
+              $nav.removeClass( 'nav-dot-current' );
+              $dot.addClass( 'nav-dot-current' );
 
-        */
-    
-    });
+            }
+
+            slitslider.jump( i + 1 );
+            return false;
+
+          } );
+
+        } );
+
+      };
+
+      return { init : init };
+
+  })();
+
+  Page.init();
+
+  /**
+   * Notes:
+   *
+   * example how to add items:
+   */
+
+  /*
+
+  var $items  = $('<div class="sl-slide sl-slide-color-2" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1"><div class="sl-slide-inner bg-1"><div class="sl-deco" data-icon="t"></div><h2>some text</h2><blockquote><p>bla bla</p><cite>Margi Clarke</cite></blockquote></div></div>');
+
+  // call the plugin's add method
+  ss.add($items);
+
+  */
+
+});
