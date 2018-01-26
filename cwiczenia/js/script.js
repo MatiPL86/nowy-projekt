c5e29edfed5104e49b6a35c1d5b6a3877fb6dfc7
MatiPL86 <31951249+MatiPL86@users.noreply.github.com>
2018-01-26 11:34:25 +0100

    2 pages still

--- cwiczenia/js/script.js
+++ cwiczenia/js/script.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     $('#pagepiling').pagepiling({
           direction: 'horizontal',
         menu: '#menu',
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-        sectionsColor: ['white', '#ee005a', '#2C3E50', '#39C', 'rgb(120, 216, 65)'],
+        anchors: ['page1', 'page2', 'page3', 'page4',],
+        sectionsColor: ['white', 'rgba(206, 186, 120, 0.822)', '#2C3E50', '#39C', 'rgb(120, 216, 65)'],
         navigation: {
             'position': 'right',
-               'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Pgae 4', 'page5']
+               'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Pgae 4',]
            },
         afterRender: function(){
             $('#pp-nav').addClass('custom');
